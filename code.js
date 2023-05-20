922ed6e97840b230d50c4471f429db279a0b5aa9
Lisa Mbuwa <mbuwalisa@gmail.com>
2023-05-20 14:10:12 +0200

    second commit

---
+++ code.js
@@ -0,0 +1,17 @@
+const screenDisplay  = document.querySelector('.screen')
+const buttons = document.querySelectorAll('button')
+
+
+
+  buttons.forEach(button =>)
+
+
+
+
+
+
+
+
+
+const wipe = ()
+ 
